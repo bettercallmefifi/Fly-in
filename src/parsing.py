dd65808926454070a0ccb60aa730604aa807ddae
Ferdaous El Idrissi <feel-idr@e1r8p8.1337.ma>
2026-07-02 19:58:08 +0100

    continue the parsing file part 3: add parse_connection

--- src/parsing.py
+++ src/parsing.py
@@ -7,6 +7,7 @@ class ParsingError(Exception):
 class Parser:
     def __init__(self, file_name: str):
         self.file_name = file_name
+        self.zone_names = set()
 
     def parsing(self):
         try:
@@ -19,32 +20,32 @@ class Parser:
         start_zone_exist = False
 
         for line in lines:
-            line = line.strip().lower()
+            line = line.strip()
             if not line or line.startswith("#"):
                 continue
-
-            elif line.startswith("nb_drones:"):
+            lower_line = line.lower()
+            if lower_line.startswith("nb_drones:"):
                 if nb_drone_exist:
                     raise ParsingError("nb_drones already exist !")
                 self.parse_nb_drones(line)
                 nb_drone_exist = True
 
-            elif line.startswith("start_hub:"):
+            elif lower_line.startswith("start_hub:"):
                 if start_zone_exist:
                     raise ParsingError("start_hub already exist !")
                 self.parse_zone(line)
                 start_zone_exist = True
 
-            elif line.startswith("end_hub:"):
+            elif lower_line.startswith("end_hub:"):
                 if end_zone_exist:
                     raise ParsingError("end_hub already exist !")
                 self.parse_zone(line)
                 end_zone_exist = True
 
-            elif line.startswith("hub:"):
+            elif lower_line.startswith("hub:"):
                 self.parse_zone(line)
 
-            elif line.startswith("connection:"):
+            elif lower_line.startswith("connection:"):
                 self.parse_connection(line)
             else:
                 raise ParsingError("There is no data !")
@@ -88,7 +89,45 @@ class Parser:
 
 
     def parse_connection(self, line: str) -> None:
-        pass
+        line = line.split(":", 1)
+        if len(line) != 2:
+            raise ParsingError("Invalid data !")
+        data_list = line[1].strip()
+        metadata_string = ""
+        if "[" in data_list:
+            if not data_list.endswith("]"):
+                raise ParsingError("Metadata most be closed with ']'!")
+
+            base_data, meta_part = data_list.split("[", 1)
+            metadata_string = meta_part.replace("]", "").strip()
+        else:
+            base_data = data_list
+        
+        base_elements = base_data.split()
+        count = 0
+        for i in base_elements:
+            if "-" == i:
+                count += 1
+
+        if count > 1 or count == 0:
+            raise ParsingError("The connection syntax forbids dashes in zone names !")
+
+        names = base_elements.split("-")
+        if len(names) != 2:
+            raise ParsingError(f"Invalid connection format: expected <name1>-<name2>, got '{base_elements}'")
+
+        name1 = names[0].strip()
+        name2 = names[1].strip()
+
+        if name1 not in self.zone_names:
+            raise ParsingError(f"Connection error: Zone '{name1}' does not exist!")
+        if name2 not in self.zone_names:
+            raise ParsingError(f"Connection error: Zone '{name2}' does not exist!")
+        if name1 == name2:
+            raise ParsingError(f"Connection error: Cannot connect zone '{name1}' to itself!")
+
+
+
 
     def valid_xy(self, x: str, y: str) -> Tuple[int, int]:
         try:
