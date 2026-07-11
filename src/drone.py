f1c5bb3882d2bbe7fb15411c9ce91451109289d9
Ferdaous El Idrissi <feel-idr@c3r5p4.1337.ma>
2026-07-11 22:15:02 +0100

    add some methods to Drone class

--- src/drone.py
+++ src/drone.py
@@ -5,3 +5,20 @@ class Drone:
         self.current_step = 0
         self.status = "waiting"
 
+    def get_current_zone(self):
+        if self.current_step < len(self.path):
+            return self.path[self.current_step]
+        return None
+
+    def get_next_zone(self):
+        if self.current_step + 1 < len(self.path):
+            return self.path[self.current_step + 1]
+        return None
+
+    def move(self):
+        if self.status != "arrived" and self.get_next_zone() is not None:
+            self.current_step += 1
+            self.status = "moving"
+            
+            if self.current_step == len(self.path) - 1:
+                self.status = "arrived"
